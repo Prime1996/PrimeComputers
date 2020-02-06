293cdf73bc385fe576b7e083137c170ba2c422bc
Prime1996 <58663185+Prime1996@users.noreply.github.com>
2020-02-06 12:17:26 +0600

    Add files via upload

---
+++ start.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="start.aspx.cs" Inherits="start" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+   #ContentPlaceHolder1_pnlProduct div{
+    float: left;
+    width: 250px;
+    height: 250px;
+    text-align: center;
+}
+
+.productImage {
+    width: 150px;
+    height: 150px;
+    vertical-align: central;
+    padding: 10px;
+    border-radius:50px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+}
+
+.productName {
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.productPrice {
+    font-family: Arial, helvetica, sans-serif;
+    color: #FFB600;
+}
+
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <marquee behavior="scroll" direction="left" style="font-size: 20px; color:#0d4386"># Upto 20% discount on Master Card Payment #</marquee>
+    <marquee behavior="scroll" direction="right" style="font-size: 20px; color:#f00"># Prebook the upcoming MSI laptops with GTX 16 Seriese GPUs #</marquee>
+    
+    <asp:Panel ID="pnlProduct" runat="server">
+    </asp:Panel>
+    <div style="clear:both;"></div>
+</asp:Content>
+
---
+++ start.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class start : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        FillPage();
+    }
+
+    private void FillPage()
+    {
+        //get a list of all products
+        ProductModel productModel = new ProductModel();
+        List<Product> products = productModel.GatAllProducts();
+
+        //existance of product
+        if (products != null)
+        {
+            //new panel with a imagebutton and 2 labels
+            foreach (Product product in products)
+            {
+                Panel productPanel = new Panel();
+                ImageButton imageButton = new ImageButton();
+                Label lblName = new Label();
+                Label lblPrice = new Label();
+
+                //childcontrol
+                imageButton.ImageUrl = "~/Images/Products/" + product.Image;
+                imageButton.CssClass = "productImage";
+                imageButton.PostBackUrl = "~/Pages/Product.aspx?id=" + product.Id;
+
+                lblName.Text = product.Name;
+                lblName.CssClass = "productName";
+
+                lblPrice.Text = "Tk. " + product.Price;
+                lblPrice.CssClass = "productPrice";
+
+                //child control to panel
+                productPanel.Controls.Add(imageButton);
+                productPanel.Controls.Add(new Literal { Text = "<br />" });
+                productPanel.Controls.Add(lblName);
+                productPanel.Controls.Add(new Literal { Text = "<br />" });
+                productPanel.Controls.Add(lblPrice);
+
+                //dynamic to static parent panel
+                pnlProduct.Controls.Add(productPanel);
+            }
+        }
+        else
+        {
+            //no product found
+            pnlProduct.Controls.Add(new Literal {Text = "No products found"});
+        }
+    }
+}
